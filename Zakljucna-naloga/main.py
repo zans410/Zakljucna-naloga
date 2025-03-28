4e35c7f72f3a2c62e86022980c615e52cf1c0a61
zans410 <staniczan1@gmail.com>
2025-03-28 08:18:41 +0100

    woah

--- Zakljucna-naloga/main.py
+++ Zakljucna-naloga/main.py
@@ -1,2 +1,5 @@
 from flask import flask, requests, render_template, redirect, url_for, jsonify
 import bcrypt
+
+#Da se ne vidi gesla ko ga vpisuješ
+hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
