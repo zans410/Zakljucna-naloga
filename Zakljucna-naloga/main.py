7108373f9f3d565f0dba6006f71549b2041ab481
Milkovic07 <jan.milkovic2@gmail.com>
2025-03-30 23:00:31 +0200

    brisnaje kode za testiranje

--- Zakljucna-naloga/main.py
+++
@@ -1,5 +0,0 @@
-from flask import flask, requests, render_template, redirect, url_for, jsonify
-import bcrypt
-
-#Da se ne vidi gesla ko ga vpisuješ
-hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
