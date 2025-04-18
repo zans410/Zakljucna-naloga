fbf4f183dadce591f6e22fc527dd357c271c71c0
Milkovic07 <jan.milkovic2@gmail.com>
2025-04-18 08:38:10 +0200

    Add files via upload
    izbolsane novejse funkcije

--- Zaključna naloga - python/main.py
+++ Zaključna naloga - python/main.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from functools import wraps
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
 app.secret_key = 'kljuc123'
@@ -8,7 +8,53 @@ app.secret_key = 'kljuc123'
 #moj google Places API ključ
 API_KEY = "AIzaSyBOW8o0C9ZML3aNkyFaDokMM4Hy5fSOsOk"
 
-users_db = {}  #sharanjevanje uporabnikov
+db = TinyDB('users.json') #databaza za uporabnike
+users_table = db.table('users') #kreiranje tabele v databazi
+activity_table = db.table('court_activity') #tabela za aktivnost na igriscih
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST']) #stran za registracijo uporabnika
+def register_user(): 
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        User = Query() 
+        if users_table.contains(User.username == username): #preverja ce so uporabniki ze v bazi
+            flash("Uporabniško ime je že v bazi. Poiskusite z drugim.")
+        #dodajanje uporabnika v bazo ce se ne obstaja
+        else: 
+            users_table.insert({'username': username, 'password': password})
+            flash(f"Uporabnik '{username}' uspešno registrtitan!")
+            return redirect(url_for('login_user'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST']) #stran za prijavo uporabnika
+def login_user(): 
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        User = Query()
+        user = users_table.search(User.username == username) #preveri ce je uporabnik v bazi
+        if user and user[0]['password'] == password:
+            session['user'] = username #shranjen uporabnik v sejo
+            flash(f"Pozdravljeni nazaj, {username}!")
+            return redirect(url_for('search_courts'))
+        else:
+            flash("Nepravilno uporabniško ime ali geslo.")
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout(): #odjava uporabnika
+    session.pop('user', None)
+    flash("Uspešno ste se odjavili.")
+    return redirect(url_for('index'))
 
 def get_coordinates(place_name):
     url = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -51,67 +97,66 @@ def get_basketball_courts(place_name, radius=5000):
                 if photo_reference:
                     photo_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_reference}&key={API_KEY}"
 
+            place_id = place["place_id"]
+            google_maps_url = f"https://www.google.com/maps/place/?q=place_id:{place_id}"
+
+            court_name = place.get("name")
+            active_users =  []
+            all_activites = activity_table.all()
+
+            for activity in all_activites:
+                if activity['court_name'] == court_name and activity['status'] == 'active':
+                    active_users.append(activity['user'])
+
             courts.append({
                 "name": place.get("name", "N/A"), 
                 "vicinity": place.get('vicinity', "Ni podatkov o lokaciji"), #dodav sem privzeto vrednost zaradi debugga
-                "photo_url": photo_url
+                "photo_url": photo_url,
+                "google_maps_url": google_maps_url, #url za gumb
+                "active_users": active_users
             })
         
         return courts
 
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs): #zahtevanje prijave
-        if "user" not in session:
-            flash("Najprej se morate prijaviti!")
-            return redirect(url_for("login_user"))
-        return f(*args, **kwargs)
-    return wrap
-
-@app.route('/register', methods=['GET', 'POST'])
-def register_user(): #stran za registracijo uporabnika
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        if username in users_db:
-            flash("Uporabniško ime že obstaja. Poskusite z drugim.")
-        else:
-            users_db[username] = password
-            flash(f"Uporabnik '{username}' uspešno registriran!")
-            return redirect(url_for('login_user'))
-    
-    return render_template('register.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login_user(): #stran za prijavo uporabnika
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        if username in users_db and users_db[username] == password:
-            session['user'] = username #shrani uporabnika v sejo
-            flash(f"Pozdravljeni nazaj, {username}!")
-            return redirect(url_for('search_courts'))
-        else:
-            flash('Napačno uporabniško ime ali geslo')
-
-    return render_template('login.html')
-
-@app.route('/logout')
-def logout(): #odjava uporabnika
-    session.pop('user', None)
-    flash("Uspešno ste se odjavili.")
-    return redirect(url_for('login_user'))
-
 @app.route('/search', methods=['GET', 'POST'])
-@login_required
 def search_courts(): #stran za iskanje košarkarskih igrišč (samo za prijavljene uporabnike)
     courts = []
+
     if request.method == 'POST':
-        place_name = request.form['place_name']
+        place_name = request.form.get('place_name')
+    elif request.method == 'GET':
+        place_name = request.args.get('place_name')
+
+    if place_name:
         courts = get_basketball_courts(place_name)
-    return render_template("search.html", courts=courts)
+
+    return render_template("search.html", courts=courts, place_name=place_name)
+
+@app.route('/join_court/<court_name>', methods=['POST'])
+def join_court(court_name):
+    user = session['user']
+    place_name = request.form.get('place_name')
+
+    activity = {
+        'court_name': court_name,
+        'user': user,
+        'status': 'active'
+    }
+    activity_table.insert(activity)
+
+    flash(f"{user} se je uspešno pridružil igrišču {court_name}.")
+    return redirect(url_for('search_courts') + f"?place_name={place_name}")
+
+@app.route('/leave_court/<court_name>', methods=['POST'])
+def leave_court(court_name):
+    user = session['user']
+    place_name = request.form.get('place_name')
+
+    activity = Query()
+    activity_table.remove((activity.court_name == court_name) & (activity.user == user))
+
+    flash(f"{user} je zapustil igrišče {court_name}.")
+    return redirect(url_for('search_courts') + f"?place_name={place_name}")
 
 if __name__ == "__main__":
     app.run(debug=True)
